99410f02639f9af4e1ff351ffbcacea1d2571e79
Stephen Cefali <scefali@sentry.io>
2022-04-24 18:00:25 -0700

    adds sentry

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,18 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://6f1e9f8dd238407185b0ecea0e2bbb51@o1218253.ingest.sentry.io/6360265",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",

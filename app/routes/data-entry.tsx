bbac255fab0f6476b784641d183bbca6044ee061
Stephen Cefali <scefali@sentry.io>
2022-04-24 18:05:02 -0700

    wrap route with Sentry

--- app/routes/data-entry.tsx
+++ app/routes/data-entry.tsx
@@ -1,15 +1,21 @@
 import { json } from "@remix-run/cloudflare";
 import type { KVNamespace } from "@cloudflare/workers-types";
 import { useLoaderData } from "@remix-run/react";
+import * as Sentry from "@sentry/react";
 
 // declare const remixcloudflare: KVNamespace;
 
 export const action = async ({ request }) => {
-  // why is request.body a ReadableStream?
-  // const reader = request.body.getReader();
-  // const data = await reader.read();
-  const data = await request.json();
-  console.log("data", data, DATAENTRY);
-  await DATAENTRY.put(data.field, data.value);
-  return json({ message: "ok" });
+  try {
+    // why is request.body a ReadableStream?
+    // const reader = request.body.getReader();
+    // const data = await reader.read();
+    const data = await request.json();
+    console.log("data", data, DATAENTRY);
+    await DATAENTRY.put(data.field, data.value);
+    return json({ message: "ok" });
+  } catch (err) {
+    Sentry.captureException(err);
+    throw err;
+  }
 };
